7482baf8c052ba2357b44264365fa3403174f3ea
Matthew Everett <matthew@mjheverett.com>
2020-08-25 10:24:15 -0400

    Working api fetch to update page

--- js/index.js
+++ js/index.js
@@ -2,10 +2,28 @@
 
 const chuckSays = document.getElementById('chuckSays');
 const refreshButton = document.getElementById('refreshQuote');
+const submitFormButton = document.getElementById('submitForm');
+const defaultCategory = "dev";
 
-refreshButton.addEventListener('click', function () {
-    const url = "https://api.chucknorris.io/jokes/random?category=dev";
+const getQuote = (category) => {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
     get(url).then(function(fetchResponse) {
         chuckSays.innerHTML = fetchResponse.value;
-    })
-})
+    });
+}
+
+refreshButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    getQuote(defaultCategory);
+});
+
+submitFormButton.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const userInput = document.getElementById('categoryInput');
+    const category = userInput.value;
+    getQuote(category);
+});
+
+(function () {
+    getQuote(defaultCategory);
+})();
