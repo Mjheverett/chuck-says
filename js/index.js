2f096932b816e20301df438d7e70c6726df30edd
Matthew Everett <matthew@mjheverett.com>
2020-08-25 12:48:12 -0400

    refactored to allow refreshing within the same category

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@
 const chuckSays = document.getElementById('chuckSays');
 const refreshButton = document.getElementById('refreshQuote');
 const submitFormButton = document.getElementById('submitForm');
-const defaultCategory = "dev";
+let category = "dev";
 
-const getQuote = (category) => {
+const getQuote = () => {
     const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
     get(url).then(function(fetchResponse) {
         chuckSays.innerHTML = fetchResponse.value;
@@ -17,8 +17,8 @@ const getCategories = () => {
     const dropdownMenu = document.getElementById('categoryInput');
     get(url).then(function (categoryArray) {
         categoryArray.map(function(category) {
-            const catOption = document.createElement('option');
             if (category != 'explicit') {
+                const catOption = document.createElement('option');
                 catOption.value = category;
                 catOption.text = category;
                 dropdownMenu.appendChild(catOption);
@@ -27,19 +27,21 @@ const getCategories = () => {
     });
 }
 
-refreshButton.addEventListener('click', function (e) {
+refreshButton.addEventListener('click', e => {
     e.preventDefault();
-    getQuote(defaultCategory);
+    getQuote();
 });
 
-submitFormButton.addEventListener('submit', function (e) {
+const getChuckQuotes = document.getElementById('getChuckQuotes');
+
+getChuckQuotes.addEventListener('submit', e => {
     e.preventDefault();
     const userInput = document.getElementById('categoryInput');
-    const category = userInput.value;
-    getQuote(category);
+    category = userInput.value;
+    getQuote();
 });
 
 (function () {
     getCategories();
-    getQuote(defaultCategory);
+    getQuote();
 })();
